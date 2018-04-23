e1062efc6a5ab1b4713ff5160a37192b95870639
HHEjie123 <35299864+HHEjie123@users.noreply.github.com>
2018-04-23 06:52:27 +0000

    Update real-pei.py

--- real-pei.py
+++ real-pei.py
@@ -1,2 +1,5 @@
 cmake ../ -DBUILD_EXAMPLES=true -DBUILD_GRAPHICAL_EXAMPLES=false
 sudo apt-get install git cmake3
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+sudo apt-get install libusb-1.0-0-dev pkg-config libgtk-3-dev
+libudev-dev
